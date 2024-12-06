ee28721ad3494bb5552291b7b02d74fd99cc18c8
PranaySingh13 <pranay.singh.1393@gmail.com>
2024-12-06 20:28:32 +0530

    Creational Design Pattern - Factory Method Design Pattern - Product
    Signed-off-by: PranaySingh13 <pranay.singh.1393@gmail.com>

---
+++ design-patterns/src/main/java/com/gk/designPatterns/creational/factoryMethod/product/Vehicle.java
@@ -0,0 +1,9 @@
+package main.java.com.gk.designPatterns.creational.factoryMethod.product;
+
+/**
+ * Product (Interface/Abstract Class): Defines the interface or abstract type of objects
+ * the factory method creates.
+ */
+public interface Vehicle {
+    void bookRides(String userName);
+}
