5bee35807e8db94aba4bfbbf8d154c07563ef78b
PranaySingh13 <pranay.singh.1393@gmail.com>
2024-12-13 16:52:12 +0530

    Creational Design Pattern - Abstract Factory Method Design Pattern - Abstract Products

---
+++ design-patterns/src/main/java/com/gk/designPatterns/creational/abstractFactoryMethod/abstractProducts/Bike.java
@@ -0,0 +1,8 @@
+package main.java.com.gk.designPatterns.creational.abstractFactoryMethod.abstractProducts;
+
+/**
+ * Abstract Products Defines a common interface for family of related products
+ */
+public interface Bike {
+    void assemble();
+}
