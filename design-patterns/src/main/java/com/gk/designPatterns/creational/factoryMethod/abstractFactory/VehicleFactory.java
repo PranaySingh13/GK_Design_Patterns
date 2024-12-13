224410609f73fe681c72e8b04ab508110069fb40
PranaySingh13 <pranay.singh.1393@gmail.com>
2024-12-13 13:22:55 +0530

    Creational Design Pattern - Factory Method Design Pattern - Abstract Factory

--- design-patterns/src/main/java/com/gk/designPatterns/creational/factoryMethod/abstractFactory/VehicleFactory.java
+++ design-patterns/src/main/java/com/gk/designPatterns/creational/factoryMethod/abstractFactory/VehicleFactory.java
@@ -1,6 +1,6 @@
-package main.java.com.gk.designPatterns.creational.factoryMethod.factoryCreator;
+package main.java.com.gk.designPatterns.creational.factoryMethod.abstractFactory;
 
-import main.java.com.gk.designPatterns.creational.factoryMethod.product.Vehicle;
+import main.java.com.gk.designPatterns.creational.factoryMethod.abstractProduct.Vehicle;
 
 /**
  * Creator (Abstract Class): Declares the factory method that returns objects of a Product type.
